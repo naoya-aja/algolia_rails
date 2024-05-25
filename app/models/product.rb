5a65ef7e0f79c587027f389d4853d049cf29d175
asakura <asakura@asasoft.info>
2024-05-25 20:42:24 +0900

    algolia設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ApplicationRecord
+  include AlgoliaSearch
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 10 }
   validates :price, numericality: { only_integer: true }
+
+  algoliasearch do
+    attributes :title, :content, :label, :price # 登録したいカラム
+    searchableAttributes ['label', 'title', 'content']
+  end
 end
