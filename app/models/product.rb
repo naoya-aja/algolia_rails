a76f567e1bd4fd0b5fec5c2fd65afcd2543636d3
asakura <asakura@asasoft.info>
2024-05-30 22:41:13 +0900

    Tag 追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   include AlgoliaSearch
+
+  has_many :taggings, dependent: :destroy
+  has_many :tags,     through: :taggings
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 10 }
   validates :price, numericality: { only_integer: true }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_062147) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_125123) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -20,4 +20,21 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_24_062147) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_taggings_on_product_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "taggings", "products"
+  add_foreign_key "taggings", "tags"
 end
